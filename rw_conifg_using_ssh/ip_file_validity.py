41c9841e52f1c76de65e869b7f4022bc11b85ede
SachinDas <107213398+i-am-sachin-root@users.noreply.github.com>
2025-01-13 15:53:14 +0530

    created ip_add_validity.py file to check is file is available or not entered by user

---
+++ rw_conifg_using_ssh/ip_file_validity.py
@@ -0,0 +1,22 @@
+import os.path
+import sys
+ 
+#Checking IP address file and content validity
+def ip_file_valid():
+
+        #take path of the file from user
+        ip_add_file =  input("\n Enter path and filename of the file which contains IP addresses: ")
+        if os.path.isfile(ip_add_file) == True:
+                print("file is valid")
+        else:
+                print("file is not valid")
+                sys.exit() #exit the program if file is not valid
+
+        # open ip_add_file and read the addresses
+        with open(ip_add_file, 'r') as f: #creating a file object and opening the file in read mode
+               f.seek(0) # moved cursor to the start of the file
+               ip_add_list = f.readlines() #reading the file line by line and storing in a list
+               f.close()
+               return ip_add_list
+
+# test :- file working as intetended 
