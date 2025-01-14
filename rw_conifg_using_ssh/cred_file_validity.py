359a8cb8bf32f8ac107ca9ff9b7abff895646a22
SachinDas <107213398+i-am-sachin-root@users.noreply.github.com>
2025-01-14 15:07:38 +0530

    created def cred_file_valid.py file to check is the file path entered by user is available or not

---
+++ rw_conifg_using_ssh/cred_file_validity.py
@@ -0,0 +1,16 @@
+# creating file which will file availablity for login credentails in devices 
+import os.path
+import sys
+
+def cred_file_valid():
+     #take path of the file from user
+        cred_file =  input("\n Enter path and filename of the file which contains username and pass: ")
+        if os.path.isfile(cred_file) == True:
+                print("file is valid")
+        else:
+                print("file is not valid")
+                sys.exit() #exit the program if file is not valid
+
+    
+
+#cred_file_valid() # calling function to test it
